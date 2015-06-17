04670bb83e8d7a2686f56895660008836f430085
juancjara <juanc.jara@pucp.pe>
2015-06-17 12:14:40 -0400

    cronjob time

--- job/index.js
+++ job/index.js
@@ -1,15 +1,19 @@
 var CronJob = require('cron').CronJob;
 
-var dailyUpdateDatabase = require('./dailyUpdateDatabase');
-var time = '0 */1 * * * *';
-//var time = '0 */2 * * *';//2 hours
+var updateDatabase = require('./dailyUpdateDatabase');
+//var time = '0 */1 * * * *';
+var time = '0 */2 * * *';//2 hours
 //var time = '00 30 3 * * *';
+
+var doOnTick = function() {
+	console.log('updateDatabase');
+	updateDatabase.start();
+};
 var job = new CronJob({
   cronTime: time,
-  onTick: function() {
-    console.log('dailyUpdateDatabase');
-    dailyUpdateDatabase.start();
-  }
+  onTick: 
 })
+
+doOnTick();
 
 job.start();
